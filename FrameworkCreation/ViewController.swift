8240b2679ac6378fcebaed2466dceaae44954197
jmuthialu <jmuthialu@yahoo..com>
2016-09-28 16:43:40 -0400

    Added RepeatString framework

--- FrameworkCreation/ViewController.swift
+++ FrameworkCreation/ViewController.swift
@@ -7,19 +7,15 @@
 //
 
 import UIKit
+import RepeatString
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+    print(Repeat123.doThree(inStr: "jay..."))
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
 
 }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Specs Sources
+  source 'https://github.com/jmuthialu/PodSpecsJM.git'
+  source 'https://github.com/CocoaPods/Specs.git'
+
+# platform :ios, '8.0'
+  use_frameworks!
+
+target 'FrameworkCreation' do
+	pod 'RepeatString'
+end
